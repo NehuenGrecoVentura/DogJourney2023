edacc7f72f30353df34309fcc9856f3818ee336b
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-06 01:13:35 -0300

    Fix canvas mercado

--- Assets/2023/Scripts/Market/MarketPlace.cs
+++ Assets/2023/Scripts/Market/MarketPlace.cs
@@ -4,21 +4,31 @@ public class MarketPlace : MonoBehaviour
 {
     private MarketManager _market;
     [SerializeField] Transform _exitPos;
+    private LocationQuest _radar;
 
     private void Awake()
     {
         _market = FindObjectOfType<MarketManager>();
+        _radar = FindObjectOfType<LocationQuest>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<Character>();
-        if (player != null) _market.OpenMarket();
+        if (player != null)
+        {
+            _market.OpenMarket();
+            _radar.StatusRadar(false);
+        }   
     }
 
     private void OnTriggerExit(Collider other)
     {
         var player = other.GetComponent<Character>();
-        if (player != null) player.gameObject.transform.position = _exitPos.position;
+        if (player != null)
+        {
+            player.gameObject.transform.position = _exitPos.position;
+            _radar.StatusRadar(true);
+        }      
     }
 }
