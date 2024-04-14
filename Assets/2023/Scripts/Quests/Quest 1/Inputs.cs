ae3f9dd6a59c1da6f59a48d5c85402f4c8609b02
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-13 23:47:27 -0300

    Tutorial inputs UI nueva

--- Assets/2023/Scripts/Quests/Quest 1/Inputs.cs
+++ Assets/2023/Scripts/Quests/Quest 1/Inputs.cs
@@ -7,13 +7,7 @@ using DG.Tweening;
 public class Inputs : MonoBehaviour
 {
     private PickAxe _axe;    
-    private bool _w, _a, _s, _d;
-
-    [SerializeField] Image[] _keys;
-    [SerializeField] TMP_Text _text;
-    [SerializeField] Button _buttonSellWoods;
     [SerializeField] GameObject[] _nextStep;
-    [SerializeField] float _timeToNextMessage;
     [SerializeField] Collider _mailQuest1;
     [SerializeField] GameObject _tutorialAddDog;
     [SerializeField] GameObject _womanInMarket;
@@ -26,6 +20,8 @@ public class Inputs : MonoBehaviour
     [Header("ICONS CONTROLS")]
     [SerializeField] Image[] _iconsControls;
     [SerializeField] Transform[] _parentsIcons;
+    [SerializeField] RectTransform _boxWASD;
+    [SerializeField] RectTransform _boxZoom;
 
     private TutorialAddDog _dog2;
 
@@ -38,27 +34,17 @@ public class Inputs : MonoBehaviour
 
     void Start()
     {
-        foreach (var item in _iconsControls)
-        {
-            item.color = new Color(0, 0, 0, 0);
-            item.DOFade(0.5f, 1f);
-        }
+        StartCoroutine(Move());
+    }
 
-        foreach (var item in _parentsIcons)
-        {
-            // Obtener la posición actual del RectTransform
-            Vector3 nuevaPosicion = item.GetComponent<RectTransform>().anchoredPosition;
+    private void NextStep(bool active)
+    {
+        foreach (var item in _nextStep)
+            item.SetActive(active);
+    }
 
-            // Ajustar la posición en el eje X
-            nuevaPosicion.x = -100;
-
-            // Aplicar la nueva posición al RectTransform
-            item.GetComponent<RectTransform>().anchoredPosition = nuevaPosicion;
-
-
-            item.transform.DOMoveX(134, 1f);
-        }
-
+    private IEnumerator Move()
+    {
         _dog2.enabled = false;
         _dog2.gameObject.SetActive(false);
         _axe.enabled = false;
@@ -68,68 +54,22 @@ public class Inputs : MonoBehaviour
         _radar.StatusRadar(false);
         _dogBall.enabled = false;
         NextStep(false);
-    }
 
-    void Update()
-    {
-        CheckKey(KeyCode.W, ref _w, _keys[0]);
-        CheckKey(KeyCode.A, ref _a, _keys[1]);
-        CheckKey(KeyCode.S, ref _s, _keys[2]);
-        CheckKey(KeyCode.D, ref _d, _keys[3]);
-
-        if (_w && _a && _s && _d)
-            TutorialSuccess();
-    }
-
-    private void TutorialSuccess()
-    {
-        foreach (var key in _keys)
-        {
-            key.color = Color.green;
-            _text.color = Color.green;
-            _text.text = "GOOD";
-        }
-    }
-
-    private void NextStep(bool active)
-    {
-        foreach (var item in _nextStep)
-            item.SetActive(active);
-    }
-
-    private void CheckKey(KeyCode key, ref bool keyState, Image keyImage)
-    {
-        switch (keyState)
-        {
-            case false:
-                if (Input.GetKeyDown(key))
-                {
-                    keyState = true;
-                    keyImage.color = Color.yellow;
-
-                    if (_w && _a && _s && _d) StartCoroutine(NextMessage());
-                }
-                break;
-
-            case true:
-                if (Input.GetKeyUp(key)) keyImage.color = Color.white;
-                break;
-        }
-    }
-
-    private IEnumerator NextMessage()
-    {
-        yield return new WaitForSeconds(_timeToNextMessage);
-
-        foreach (var item in _parentsIcons)
-        {
-            item.transform.DOMoveX(-1000, 1f);
-        }
-
-        NextStep(true);
+        _boxZoom.gameObject.SetActive(false);
+        _boxWASD.DOAnchorPosY(-202f, 1f);
+        yield return new WaitForSeconds(3f);
+        _boxWASD.DOAnchorPosY(-1000f, 1f);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(_boxWASD.gameObject);
+        _boxZoom.gameObject.SetActive(true);
+        _boxZoom.DOAnchorPosY(-202f, 1f);
+        yield return new WaitForSeconds(3f);
+        _boxZoom.DOAnchorPosY(-1000f, 1f);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(_boxZoom.gameObject);
         _mailQuest1.enabled = true;
         _radar.StatusRadar(true);
         _radar.target = _nextPos;
-        Destroy(gameObject, 1.5f);
+        Destroy(gameObject);
     }
 }
