f1db89499958407782b34aa98ce7abb0e6147754
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-17 17:45:13 -0300

    Mensaje UI sigilo

--- Assets/Scripts/2022/Messages in screen/MessageStealth.cs
+++ Assets/Scripts/2022/Messages in screen/MessageStealth.cs
@@ -5,7 +5,7 @@ using DG.Tweening;
 public class MessageStealth : MonoBehaviour
 {
     [Header("MESSAGE")]
-    [SerializeField] GameObject _boxMessage;
+    [SerializeField] RectTransform _boxMessage;
     private Collider _myCol;
 
     private void Awake()
@@ -16,7 +16,9 @@ public class MessageStealth : MonoBehaviour
     void Start()
     {
         _boxMessage.gameObject.SetActive(false);
-        _boxMessage.transform.DOScale(0, 0);
+        _boxMessage.DOAnchorPosY(-1000f, 0f);
+        _boxMessage.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,12 +29,13 @@ public class MessageStealth : MonoBehaviour
 
     private IEnumerator Display()
     {
+        
         Destroy(_myCol);
         _boxMessage.gameObject.SetActive(true);
-        _boxMessage.transform.DOScale(0.8f, 0.5f);
+        _boxMessage.DOAnchorPosY(-100f, 0.5f);
         yield return new WaitForSeconds(4f);
-        _boxMessage.GetComponent<RectTransform>().DOMoveY(-1000, 1f);
-        Destroy(_boxMessage, 2f);
-        Destroy(this);
+        _boxMessage.DOAnchorPosY(-1000f, 0.5f);
+        Destroy(_boxMessage.transform.parent.gameObject, 2f);
+        Destroy(this, 2f);
     }
 }
