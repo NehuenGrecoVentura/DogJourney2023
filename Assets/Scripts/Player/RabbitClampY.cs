6a5ea4119fb8ed49a078a20044e0ff163e4b0464
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-20 17:23:56 -0300

    Limpieza Parte 4

--- Assets/Scripts/Player/RabbitClampY.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class RabbitClampY : MonoBehaviour
-{
-    void Update()
-    {
-        transform.position = new Vector3(transform.position.x, 0.137f, transform.position.z);
-    }
-}
