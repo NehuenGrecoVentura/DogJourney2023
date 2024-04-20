08c88046b7ef678712cabe531f3d523bf78ae373
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-20 17:50:04 -0300

    Limpieza Parte 9

--- Assets/Scripts/2022/IconUp2022.cs
+++ Assets/Scripts/2022/IconUp2022.cs
@@ -3,23 +3,5 @@ using UnityEngine.UI;
 
 public class IconUp2022 : MonoBehaviour
 {
-    public Image iconDog;
-    private NewDogUp _isUp;
-
-    private void Start()
-    {
-        iconDog.gameObject.SetActive(false);
-        _isUp = FindObjectOfType<NewDogUp>();
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player" && _isUp.isUp) iconDog.gameObject.SetActive(false);
-        else if (other.gameObject.tag == "Player" && !_isUp.isUp) iconDog.gameObject.SetActive(true);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player") iconDog.gameObject.SetActive(false);
-    }
+   
 }
