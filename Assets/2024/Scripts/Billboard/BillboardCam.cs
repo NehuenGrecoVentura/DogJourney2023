d54cf84853755935a581b583fb9a1fba780e95d3
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-26 20:21:25 -0300

    Fix billboard troncos

--- Assets/2024/Scripts/Billboard/BillboardCam.cs
+++ Assets/2024/Scripts/Billboard/BillboardCam.cs
@@ -11,27 +11,27 @@ public class BillboardCam : MonoBehaviour
 
     void Update()
     {
-        //// Calcula la rotación necesaria para mirar hacia la cámara
-        //Vector3 lookDirection = _camPos.position - transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(lookDirection);
+        // Calcula la rotación necesaria para mirar hacia la cámara
+        Vector3 lookDirection = _camPos.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection);
 
 
-        //// Extrae las rotaciones en los ejes individuales
-        //float rotationX = rotation.eulerAngles.x;
-        //float rotationY = rotation.eulerAngles.y;
-        //float rotationZ = rotation.eulerAngles.z;
+        // Extrae las rotaciones en los ejes individuales
+        float rotationX = rotation.eulerAngles.x;
+        float rotationY = rotation.eulerAngles.y;
+        float rotationZ = rotation.eulerAngles.z;
 
-        //// Aplica la rotación en los ejes X, Y, Z
-        ////transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
+        // Aplica la rotación en los ejes X, Y, Z
         //transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
     }
 
-    void LateUpdate()
-    {
-        // Calcula la dirección hacia la cámara del jugador
-        Vector3 directionToCamera = transform.position - _camPos.transform.position;
+    //void LateUpdate()
+    //{
+    //    // Calcula la dirección hacia la cámara del jugador
+    //    Vector3 directionToCamera = transform.position - _camPos.transform.position;
 
-        // Apunta hacia la cámara, pero invierte el eje Z para corregir la inversión
-        transform.rotation = Quaternion.LookRotation(-directionToCamera, Vector3.up);
-    }
+    //    // Apunta hacia la cámara, pero invierte el eje Z para corregir la inversión
+    //    transform.rotation = Quaternion.LookRotation(-directionToCamera, Vector3.up);
+    //}
 }
