8dceaa4cd54fd20a6e7a87cb8c153832d9d2b3ef
Nehuen <nehuen.greco@davinci.edu.ar>
2024-04-27 15:33:25 -0300

    Pesca fix movimiento player

--- Assets/2024/Scripts/Isla 1/Pesca/Fishing.cs
+++ Assets/2024/Scripts/Isla 1/Pesca/Fishing.cs
@@ -6,15 +6,56 @@ using TMPro;
 
 public class Fishing : MonoBehaviour
 {
-    private FishingMinigame _fishing;
+    [Header("INTERACT")]
+    [SerializeField] GameObject _iconInteract;
+    [SerializeField] KeyCode _keyInteract = KeyCode.F;
+
+    [Header("CAMERAS")]
     [SerializeField] Camera _camRender;
     [SerializeField] Camera _myCam;
+    private CameraOrbit _camPlayer;
+
+    [Header("FISHING")]
+    [SerializeField] int _totalAmount = 3;
+    private FishingMinigame _fishing;
+    private Character _player;
     private Collider _myCol;
+    private bool _isActive = false;
 
     private void Awake()
     {
         _myCol = GetComponent<Collider>();
+
         _fishing = FindObjectOfType<FishingMinigame>();
+        _player = FindObjectOfType<Character>();
+        _camPlayer = FindObjectOfType<CameraOrbit>();
+    }
+
+    private void Start()
+    {
+        _myCam.gameObject.SetActive(false);
+        _iconInteract.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isActive && _fishing.fishedPicked == _totalAmount) EndGame();
+    }
+
+    private void EndGame()
+    {
+        _fishing.Quit();
+        _player.speed = _player.speedAux;
+        _player.FreezePlayer(RigidbodyConstraints.FreezeRotation);
+        _camPlayer.gameObject.SetActive(true);
+        Destroy(_myCam.gameObject);
+        _isActive = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null && !_isActive) _iconInteract.SetActive(true);
     }
 
     private void OnTriggerStay(Collider other)
@@ -22,24 +63,31 @@ public class Fishing : MonoBehaviour
         var player = other.GetComponent<Character>();
         if (player != null)
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.F) && !_isActive)
             {
                 StartCoroutine(StartMiniGame());
             }
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null && !_isActive) _iconInteract.SetActive(false);
+    }
+
     private IEnumerator StartMiniGame()
     {
+        _camPlayer.gameObject.SetActive(false);
+        _myCam.gameObject.SetActive(true);
+        _player.speed = 0;
+        _player.FreezePlayer(RigidbodyConstraints.FreezeAll);
+        _isActive = true;
         Destroy(_myCol);
+        Destroy(_iconInteract);
         _fishing.start = true;
         yield return new WaitForSeconds(2f);
         _fishing.Gaming = true;
         _camRender.enabled = true;
     }
-
-
-
-
-
 }
