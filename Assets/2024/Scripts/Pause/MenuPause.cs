209379a4fccf307c8eddac6ecc207628bc15c1fe
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-07 20:10:58 -0300

    Fix reiniciar

--- Assets/2024/Scripts/Pause/MenuPause.cs
+++ Assets/2024/Scripts/Pause/MenuPause.cs
@@ -59,6 +59,7 @@ public class MenuPause : MonoBehaviour
 
     public void Restart()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
