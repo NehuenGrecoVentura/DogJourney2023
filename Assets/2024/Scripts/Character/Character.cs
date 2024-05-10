93f5543726310b06e6b57cdefd00696c449c16e4
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-10 00:42:34 -0300

    Print colision quitado

--- Assets/2024/Scripts/Character/Character.cs
+++ Assets/2024/Scripts/Character/Character.cs
@@ -125,9 +125,4 @@ public class Character : MonoBehaviour
         var pick = other.GetComponent<IPick>();
         if (pick != null) pick.Pick();
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        print(collision.gameObject.name);
-    }
 }
