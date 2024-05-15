346223608d1f8809286d3e038c67119a8cc9204a
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-15 20:31:13 -0300

    Arbol particulas madera

--- Assets/2024/Scripts/Trees Regeneratives/TreeFall.cs
+++ Assets/2024/Scripts/Trees Regeneratives/TreeFall.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class TreeFall : MonoBehaviour
 {
@@ -9,33 +10,84 @@ public class TreeFall : MonoBehaviour
     [SerializeField] ParticleSystem _smokeParticle;
     [SerializeField] SaplingTree _sapling;
 
+    //[SerializeField] GameObject _particlesParent;
+    private Vector3[] _initialPos;
+    private GameObject[] _particles;
+    [SerializeField] GameObject _decal;
     public bool _isFall;
 
     void Start()
     {
-        _initialTime = _timeToOff;
+        //_initialTime = _timeToOff;
+
+        // Obtén el número de hijos del GameObject padre
+        int childCount = transform.childCount;
+
+        // Inicializa el array de partículas con el tamaño del número de hijos
+        _particles = new GameObject[childCount];
+
+        // Inicializa el array de posiciones iniciales con el tamaño del número de hijos
+        _initialPos = new Vector3[childCount];
+
+        // Itera sobre los hijos del GameObject padre
+        for (int i = 0; i < childCount; i++)
+        {
+            // Obtiene cada hijo y lo almacena en el array de partículas
+            _particles[i] = transform.GetChild(i).gameObject;
+
+            // Almacena la posición inicial de cada hijo
+            _initialPos[i] = _particles[i].transform.position;
+        }
     }
 
-    private void Update()
+
+    private IEnumerator Timer()
     {
-        if (_isFall)
-        {
-            _timeToOff -= Time.deltaTime;
+        _decal.SetActive(false);
+        _trunks.gameObject.SetActive(true);
 
-            if (_timeToOff <= 0)
-            {
-                _timeToOff = 0;
-                _smokeParticle.Play();
-                _trunks.gameObject.SetActive(true);
-                _sapling.gameObject.SetActive(true);
-                _isFall = false;
-            }
+        yield return new WaitForSeconds(_timeToOff);
+        //_treeComplete.gameObject.SetActive(true);
+
+        // Restaura las posiciones iniciales de las partículas
+        for (int i = 0; i < _particles.Length; i++)
+        {
+            _particles[i].transform.position = _initialPos[i];
         }
 
-        else
-        {
-            _timeToOff = _initialTime;
-            gameObject.SetActive(false);
-        }
+        _sapling.gameObject.SetActive(true);
+        
+        gameObject.SetActive(false);
     }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Timer());
+    }
+
+
+
+
+    //private void Update()
+    //{
+    //    if (_isFall)
+    //    {
+    //        _timeToOff -= Time.deltaTime;
+
+    //        if (_timeToOff <= 0)
+    //        {
+    //            _timeToOff = 0;
+    //            _smokeParticle.Play();
+    //            _trunks.gameObject.SetActive(true);
+    //            _sapling.gameObject.SetActive(true);
+    //            _isFall = false;
+    //        }
+    //    }
+
+    //    else
+    //    {
+    //        _timeToOff = _initialTime;
+    //        gameObject.SetActive(false);
+    //    }
+    //}
 }
