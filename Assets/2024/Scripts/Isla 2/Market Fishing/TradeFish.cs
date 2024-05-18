6784c7f6dc1378ed515998d212635c665c06f608
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-18 20:19:55 -0300

    Puesto en escena de juego

--- Assets/2024/Scripts/Isla 2/Market Fishing/TradeFish.cs
+++ Assets/2024/Scripts/Isla 2/Market Fishing/TradeFish.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class TradeFish : MonoBehaviour
 {
     [SerializeField] bool[] _trades;
+    [SerializeField] GameObject _canvas;
+    private CharacterInventory _inventory;
+    private Collider _myCol;
+    private MenuPause _pause;
+    private Character _player;
 
+    [Header("INTERACT")]
+    [SerializeField] GameObject _iconInteract;
+    [SerializeField] KeyCode _keyInteract = KeyCode.F;
+
+    [Header("SPRITES")]
     [SerializeField] Sprite _spriteCommonFish;
     [SerializeField] Sprite _spriteSpecialFish;
     [SerializeField] Sprite _spriteFlowers;
     [SerializeField] Sprite _spriteNail;
     [SerializeField] Sprite _spriteApple;
     [SerializeField] Sprite _spriteBait;
+
+    [Header("TEXTS INVENTORY")]
+    [SerializeField] TMP_Text _txtInvFishCommon;
+    [SerializeField] TMP_Text _txtInvSpecialFish;
+    [SerializeField] TMP_Text _txtInvBait;
 
     [Header("TEXT VALUES")]
     [SerializeField] TMP_Text _txtValueBait;
@@ -30,6 +46,59 @@ public class TradeFish : MonoBehaviour
     [SerializeField] TMP_Text _txtAmountNormalFlower;
     [SerializeField] TMP_Text _txtAmountSpecialFlower;
     [SerializeField] TMP_Text _txtAmountNail;
+
+    private void Awake()
+    {
+        _myCol = GetComponent<Collider>();
+
+        _inventory = FindObjectOfType<CharacterInventory>();
+        _pause = FindObjectOfType<MenuPause>();
+        _player = FindObjectOfType<Character>();
+    }
+
+    private void Start()
+    {
+        _iconInteract.transform.DOScale(0f,0f);
+        _canvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        CheckItemsInInventory();
+    }
+
+    private void CheckItemsInInventory()
+    {
+        if (_canvas.activeSelf)
+        {
+            _txtInvFishCommon.text = "x " + _inventory.fishes.ToString();
+            _txtInvSpecialFish.text = "x " + _inventory.specialFishes.ToString();
+            _txtInvBait.text = "x " + _inventory.baits.ToString();
+        }
+
+        else return;
+    }
+
+    private void OpenMarket()
+    {
+        _iconInteract.transform.DOScale(0f, 0.5f);
+        _myCol.enabled = false;
+        _player.FreezePlayer();
+        _pause.Freeze();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        _canvas.SetActive(true);
+    }
+    
+    public void CloseMarket()
+    {
+        _myCol.enabled = true;
+        _player.DeFreezePlayer();
+        _pause.Defreeze();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _canvas.SetActive(false);
+    }
 
     private void TradeItem(int index)
     {
@@ -147,7 +216,26 @@ public class TradeFish : MonoBehaviour
         _txtAmountNail.GetComponentInChildren<Image>().sprite = _spriteNail;
 
         _txtValueApple.text = "$ 10";
-        _txtAmountApple.text = "x 1 = 2 Apples";
+        _txtAmountApple.text = "x 1 = 2";
         _txtAmountApple.GetComponentInChildren<Image>().sprite = _spriteApple;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null) _iconInteract.transform.DOScale(0.05f, 0.5f);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null && Input.GetKeyDown(_keyInteract)) 
+            OpenMarket();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null) _iconInteract.transform.DOScale(0f, 0.5f);
     }
 }
