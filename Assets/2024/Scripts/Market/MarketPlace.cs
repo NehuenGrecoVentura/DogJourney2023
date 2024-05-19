ee9f55c804eb666e4549fa7132c3dc93972256e8
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-19 15:34:46 -0300

    Fix mercado salida

--- Assets/2024/Scripts/Market/MarketPlace.cs
+++ Assets/2024/Scripts/Market/MarketPlace.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class MarketPlace : MonoBehaviour
 {
     private MarketManager _market;
-    [SerializeField] Transform _exitPos;
+    [SerializeField] Transform _exitPos1;
+    [SerializeField] Transform _exitPos2;
     private LocationQuest _radar;
     private MenuPause _pause;
+
+    public bool market1 = false;
+    public bool market2 = false;
 
     private void Awake()
     {
@@ -23,7 +27,23 @@ public class MarketPlace : MonoBehaviour
             _market.isShopping = true;
             _market.OpenMarket();
             _radar.StatusRadar(false);
-        }   
+
+
+            if (gameObject.name == "Market 1")
+            {
+                player.gameObject.transform.position = _exitPos1.position;
+                market1 = true;
+            }
+
+            else
+            {
+                player.gameObject.transform.position = _exitPos2.position;
+                market2 = true;
+            }
+
+
+
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -31,14 +51,41 @@ public class MarketPlace : MonoBehaviour
         var player = other.GetComponent<Character>();
         if (player != null)
         {
-            player.gameObject.transform.position = _exitPos.position;
+
+            if (gameObject.name == "Market 1")
+            {
+                player.gameObject.transform.position = _exitPos1.position;
+                //market1 = false;
+            }
+
+            else
+            {
+                player.gameObject.transform.position = _exitPos2.position;
+                //market2 = false;
+            }
+
+
+
+
             _radar.StatusRadar(true);
             _market.isShopping = false;
-        }      
+        }
     }
 
     public void SetPlayerPos(Character player)
     {
-        player.gameObject.transform.position = _exitPos.position;
+        //player.gameObject.transform.position = _exitPos.position;
+
+        if (market1)
+        {
+            player.gameObject.transform.position = _exitPos1.position;
+            market1 = false;
+        }
+
+        if (market2)
+        {
+            player.gameObject.transform.position = _exitPos2.position;
+            market2 = false;
+        }
     }
 }
