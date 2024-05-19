cd66b7d3935724342d693e331f655ad84ce09bef
Nehuen <nehuen.greco@davinci.edu.ar>
2024-05-19 00:33:30 -0300

    Limpieza codigo activacion puzzle conejo

--- Assets/2024/Scripts/Quests/Quest 2/AreaQuest2.cs
+++ Assets/2024/Scripts/Quests/Quest 2/AreaQuest2.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
-using TMPro;
-using DG.Tweening;
 using System.Collections;
 
 public class AreaQuest2 : MonoBehaviour
 {
     [Header("CINEMATIC")]
     [SerializeField] GameObject _cinematicRabbit;
-    private CameraOrbit _camPlayer;
     [SerializeField] Camera _camBuild;
     [SerializeField] Build _iconBuild;
+    private CameraOrbit _camPlayer;
 
     [Header("MESSAGE")]
-    [SerializeField] RectTransform _boxMessage;
-    [SerializeField] TMP_Text _textMessage;
-    [SerializeField] TMP_Text _textName;
     [SerializeField, TextArea(4,6)] string _message;
     [SerializeField] string _name = "Tip";
     private Character _player;
@@ -45,38 +40,6 @@ public class AreaQuest2 : MonoBehaviour
             StartCoroutine(FocusPuzzle());
         }
     }
-
-    //private IEnumerator FocusPuzzle()
-    //{
-    //    _firstContact = true;
-    //    _textMessage.text = _message;
-    //    _textName.text = _name;
-
-    //    _boxMessage.localScale = new Vector3(1, 1, 1);
-    //    _boxMessage.DOAnchorPosY(-1000f, 0f);
-    //    _camPlayer.gameObject.SetActive(false);
-    //    _cinematicRabbit.SetActive(true);
-
-    //    yield return new WaitForSeconds(1.5f);
-    //    _boxMessage.gameObject.SetActive(true);
-    //    _boxMessage.DOAnchorPosY(70f, 0.5f);
-
-    //    yield return new WaitForSeconds(3f);
-    //    Destroy(_cinematicRabbit);
-    //    _camBuild.gameObject.SetActive(true);
-    //    _iconBuild.gameObject.SetActive(true);
-
-    //    yield return new WaitForSeconds(3f);
-    //    Destroy(_camBuild.gameObject);
-    //    _camPlayer.gameObject.SetActive(true);
-    //    _player.speed = _player.speedAux;
-    //    _player.DeFreezePlayer();
-    //    _boxMessage.DOAnchorPosY(-1000f, 0.5f);
-
-    //    yield return new WaitForSeconds(0.6f);
-    //    _boxMessage.gameObject.SetActive(false);
-    //    Destroy(this);
-    //}
 
     private IEnumerator FocusPuzzle()
     {
