8726f04d3cd453437349cb0e07f632233c3302f3
Nehuen <nehuen.greco@davinci.edu.ar>
2024-06-01 22:24:31 -0300

    Fix caja en carrito

--- Assets/2024/Scripts/Trolley/TrolleyWood.cs
+++ Assets/2024/Scripts/Trolley/TrolleyWood.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class TrolleyWood : MonoBehaviour
 {
     [SerializeField] GameObject[] _trunksAmount;
-    [SerializeField] GameObject _boxUpgrade;
+    [SerializeField] GameObject[] _boxAmount;
+    //[SerializeField] GameObject _boxUpgrade;
     private CharacterInventory _inventory;
 
     private void Awake()
@@ -15,18 +16,42 @@ public class TrolleyWood : MonoBehaviour
     {
         foreach (var trunk in _trunksAmount)
             trunk.SetActive(false);
+
+        foreach (var box in _boxAmount)
+        {
+            box.SetActive(false);
+        }
+            
+
     }
 
     void Update()
     {
-        if (_inventory.upgradeLoot) _boxUpgrade.SetActive(true);
-        else _boxUpgrade.SetActive(false);
+        //if (_inventory.upgradeLoot) _boxUpgrade.SetActive(true);
+        //else _boxUpgrade.SetActive(false);
 
         if (_inventory.greenTrees > 0 && _inventory.greenTrees < 20 || _inventory.purpleTrees > 0 && _inventory.purpleTrees < 20)
         {
             _trunksAmount[0].SetActive(true);
             _trunksAmount[1].SetActive(false);
             _trunksAmount[2].SetActive(false);
+
+            if (_inventory.upgradeLoot)
+            {
+                _boxAmount[0].SetActive(false);
+                _boxAmount[1].SetActive(true);
+                _boxAmount[2].SetActive(false);
+                _boxAmount[3].SetActive(false);
+
+            }
+                
+            else
+            {
+                foreach (var box in _boxAmount)
+                {
+                    box.SetActive(false);
+                }
+            }
         }
 
         else if (_inventory.greenTrees >= 20 && _inventory.greenTrees < 30 || _inventory.purpleTrees >= 20 && _inventory.purpleTrees < 30)
@@ -34,12 +59,71 @@ public class TrolleyWood : MonoBehaviour
             _trunksAmount[0].SetActive(true);
             _trunksAmount[1].SetActive(true);
             _trunksAmount[2].SetActive(false);
+            _boxAmount[1].SetActive(false);
+
+            if (_inventory.upgradeLoot)
+            {
+                _boxAmount[0].SetActive(false);
+                _boxAmount[1].SetActive(false);
+                _boxAmount[2].SetActive(true);
+                _boxAmount[3].SetActive(false);
+
+            }
+
+            else
+            {
+                foreach (var box in _boxAmount)
+                {
+                    box.SetActive(false);
+                }
+            }
         }
 
         else if (_inventory.greenTrees >= 30 || _inventory.purpleTrees >= 30)
         {
             foreach (var trunk in _trunksAmount)
                 trunk.SetActive(true);
+
+            if (_inventory.upgradeLoot)
+            {
+                _boxAmount[0].SetActive(false);
+                _boxAmount[1].SetActive(false);
+                _boxAmount[2].SetActive(false);
+                _boxAmount[3].SetActive(true);
+
+            }
+
+            else
+            {
+                foreach (var box in _boxAmount)
+                {
+                    box.SetActive(false);
+                }
+            }
+        }
+
+        else if (_inventory.greenTrees == 0)
+        {
+            foreach (var trunk in _trunksAmount)
+                trunk.SetActive(false);
+
+
+            if (_inventory.upgradeLoot)
+            {
+                _boxAmount[0].SetActive(true);
+                _boxAmount[1].SetActive(false);
+                _boxAmount[2].SetActive(false);
+                _boxAmount[3].SetActive(false);
+
+            }
+
+            else
+            {
+                foreach (var box in _boxAmount)
+                {
+                    box.SetActive(false);
+                }
+            }
         }
     }
 }
