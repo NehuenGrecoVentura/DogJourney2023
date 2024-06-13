1e220e3873b12a32d37447f67e62f6bcd67fac60
Nehuen <nehuen.greco@davinci.edu.ar>
2024-06-13 20:11:44 -0300

    Logica activacion quest zona 3

---
+++ Assets/2024/Scripts/Zone 3/NPCZone3.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class NPCZone3 : MonoBehaviour
+{
+    [Header("INTERACT")]
+    [SerializeField] Collider _myCol;
+    [SerializeField] KeyCode _keyInteract = KeyCode.F;
+    [SerializeField] GameObject _iconInteract;
+    [SerializeField] Animator _myAnim;
+
+    [Header("DIALOGUE")]
+    [SerializeField] Dialogue _dialogue;
+    [SerializeField, TextArea(4, 6)] string[] _lines;
+    [SerializeField] TMP_Text _txtName;
+    [SerializeField] string _nameNPC;
+    [SerializeField] Button _buttonConfirm;
+
+    [Header("AUDIO")]
+    [SerializeField] AudioSource _myAudio;
+    [SerializeField] AudioClip _soundConfirm;
+
+    [Header("QUEST")]
+    private bool _questActive = false;
+    private bool _questCompleted = false;
+
+    void Start()
+    {
+        _dialogue.gameObject.SetActive(false);
+        _iconInteract.transform.DOScale(0f, 0f);
+    }
+
+    private void Confirm()
+    {
+        _myCol.enabled = false;
+        _dialogue.canTalk = false;
+        _dialogue.Close();
+        _iconInteract.transform.DOScale(0f, 0.5f);
+        _myAudio.PlayOneShot(_soundConfirm);
+        _myAnim.SetBool("Quest", true);
+        _questActive = true;
+    }
+
+    private void SetDialogue()
+    {
+        _iconInteract.transform.DOScale(0.01f, 0.5f);
+        _buttonConfirm.onClick.AddListener(() => Confirm());
+
+        if (!_questActive)
+        {
+            for (int i = 0; i < _dialogue._lines.Length; i++)
+                _dialogue._lines[i] = _lines[i];
+        }
+
+        _dialogue.gameObject.SetActive(true);
+        _dialogue.Set(_nameNPC);
+        _dialogue.playerInRange = true;
+        _dialogue.canTalk = true;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null)
+        {
+            if (_myCol.enabled && !_questActive && !_questCompleted) SetDialogue();
+            else if (_questCompleted) _iconInteract.transform.DOScale(0.01f, 0.5f);
+        }
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    var player = other.GetComponent<Character>();
+    //    if (player != null && _questCompleted && Input.GetKeyDown(_keyInteract))
+    //        StartCoroutine(Ending(player));
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<Character>();
+        if (player != null)
+        {
+            _dialogue.playerInRange = false;
+            _iconInteract.transform.DOScale(0f, 0.5f);
+        }
+    }
+}
