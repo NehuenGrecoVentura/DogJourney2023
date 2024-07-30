b2b1882dc06fec19d1c5bd4b3d53ba9bb8c427be
Nehuen <nehuen.greco@davinci.edu.ar>
2024-07-30 00:55:07 -0300

    Explicacion corte perfecto

--- Assets/2024/Scripts/Quests/Quest 1/CinematicTrunks.cs
+++ Assets/2024/Scripts/Quests/Quest 1/CinematicTrunks.cs
@@ -8,6 +8,7 @@ public class CinematicTrunks : MonoBehaviour
 {
     [SerializeField] Camera _camTrunks;
     [SerializeField] GameObject _boxMessage;
+    [SerializeField] GameObject _boxMessageSpace;
     [SerializeField] Image _iconMessage;
     [SerializeField] Sprite _iconSpaceBar;
     [SerializeField] TMP_Text _messageText;
@@ -35,22 +36,46 @@ public class CinematicTrunks : MonoBehaviour
 
     private IEnumerator Play()
     {
+        //_myAudio.Stop();
+        //_questUI.UIStatus(false);
+        //_camTrunks.gameObject.SetActive(true);
+        //_camPlayer.gameObject.SetActive(false);
+        //_player.speed = 0;
+        //_player.FreezePlayer();
+        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
+        //yield return new WaitForSeconds(2f);
+        //_myAudio.Play();
+        //_boxMessage.SetActive(true);
+        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-170f, 1f);
+        //yield return new WaitForSeconds(4f);
+        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 0.5f);
+        //_camTrunks.gameObject.SetActive(false);
+        //_camPlayer.gameObject.SetActive(true);
+        //_player.speed = _player.speedAux;
+        //_player.DeFreezePlayer();
+        //_questUI.UIStatus(true);
+        //Destroy(gameObject);
+
         _myAudio.Stop();
         _questUI.UIStatus(false);
         _camTrunks.gameObject.SetActive(true);
         _camPlayer.gameObject.SetActive(false);
-        _player.speed = 0;
+        _boxMessageSpace.gameObject.SetActive(false);
         _player.FreezePlayer();
         _boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
+        _boxMessageSpace.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
         yield return new WaitForSeconds(2f);
         _myAudio.Play();
         _boxMessage.SetActive(true);
         _boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-170f, 1f);
         yield return new WaitForSeconds(4f);
-        _boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 0.5f);
+        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 0.5f);
+        _boxMessage.SetActive(false);
+        _boxMessageSpace.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
         _camTrunks.gameObject.SetActive(false);
         _camPlayer.gameObject.SetActive(true);
-        _player.speed = _player.speedAux;
         _player.DeFreezePlayer();
         _questUI.UIStatus(true);
         Destroy(gameObject);
