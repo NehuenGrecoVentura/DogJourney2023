73bdecc2d36226ab9fb173ec0c97dc820ec2b743
Nehuen <nehuen.greco@davinci.edu.ar>
2024-08-02 15:10:13 -0300

    Saltear explicacion talar

--- Assets/2024/Scripts/Quests/Quest 1/CinematicTrunks.cs
+++ Assets/2024/Scripts/Quests/Quest 1/CinematicTrunks.cs
@@ -20,6 +20,7 @@ public class CinematicTrunks : MonoBehaviour
     private CameraOrbit _camPlayer;
     private QuestUI _questUI;
     private AudioSource _myAudio;
+    private bool _cinPlay = false;
 
     private void Awake()
     {
@@ -34,28 +35,28 @@ public class CinematicTrunks : MonoBehaviour
         StartCoroutine(Play());
     }
 
+    private void Update()
+    {
+        if (_cinPlay)
+        {
+            if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+                SkipCinematic();
+        }
+    }
+
+    private void SkipCinematic()
+    {
+        StopCoroutine(Play());
+        _camTrunks.gameObject.SetActive(false);
+        _camPlayer.gameObject.SetActive(true);
+        _player.DeFreezePlayer();
+        _questUI.UIStatus(true);
+        Destroy(gameObject);
+    }
+
     private IEnumerator Play()
     {
-        //_myAudio.Stop();
-        //_questUI.UIStatus(false);
-        //_camTrunks.gameObject.SetActive(true);
-        //_camPlayer.gameObject.SetActive(false);
-        //_player.speed = 0;
-        //_player.FreezePlayer();
-        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
-        //yield return new WaitForSeconds(2f);
-        //_myAudio.Play();
-        //_boxMessage.SetActive(true);
-        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-170f, 1f);
-        //yield return new WaitForSeconds(4f);
-        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 0.5f);
-        //_camTrunks.gameObject.SetActive(false);
-        //_camPlayer.gameObject.SetActive(true);
-        //_player.speed = _player.speedAux;
-        //_player.DeFreezePlayer();
-        //_questUI.UIStatus(true);
-        //Destroy(gameObject);
-
+        _cinPlay = true;
         _myAudio.Stop();
         _questUI.UIStatus(false);
         _camTrunks.gameObject.SetActive(true);
@@ -64,12 +65,13 @@ public class CinematicTrunks : MonoBehaviour
         _player.FreezePlayer();
         _boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
         _boxMessageSpace.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 1f);
+        
         yield return new WaitForSeconds(2f);
         _myAudio.Play();
         _boxMessage.SetActive(true);
         _boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-170f, 1f);
+        
         yield return new WaitForSeconds(4f);
-        //_boxMessage.GetComponent<RectTransform>().DOAnchorPosY(-1000f, 0.5f);
         _boxMessage.SetActive(false);
         _boxMessageSpace.SetActive(true);
 
